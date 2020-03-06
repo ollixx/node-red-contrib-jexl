cfb986139514b837b8c051fbc8179cf7f74f159a
Oliver Charlet <oliver.charlet@clarities.de>
2020-03-06 11:21:00 +0100

    initial checkin. node is functional

---
+++ jexl/jexl.js
@@ -0,0 +1,30 @@
+
+module.exports = function (RED) {
+
+    RED.nodes.registerType("jexl", pdfmake);
+    function pdfmake(config) {
+        var node = this;
+
+        // Create our node and event handler
+        RED.nodes.createNode(this, config);
+
+        this.on("input", function (msg) {
+
+            var jexl = require('jexl/lib/jexl.js');
+            try {
+                msg.payload = jexl.evalSync(msg.payload, msg.context);
+                if (msg.payload === undefined) {
+                    node.status({ fill: "yellow", shape: "ring", text: "-> undefined"});
+                } else {
+                    node.status({});
+                }
+                this.send(msg);
+            } catch (err) {
+                node.error("error parsing jexl: " + err);
+                node.status({ fill: "red", shape: "ring", text: err.message });
+            }
+
+        });
+
+    }
+} 
